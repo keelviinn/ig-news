91ab91bcec77014003c66c01034938c8c68e0bd4
Kelvin Oliveira <keelviinn@gmail.com>
2022-11-06 12:05:53 +0000

    remove console.log

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -7,8 +7,6 @@ import styles from './styles.module.scss';
 export function SignInButton() {
   const { data: session, status } = useSession();
 
-  console.log({ session, status });
-
   return status === "authenticated" ? (
     <button
       type="button"
